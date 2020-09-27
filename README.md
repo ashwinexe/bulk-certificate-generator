8b9ffcda2adec3c16aa5ace84d6576f29dc7fd9a
ashwinexe <kumarshwin2603@gmail.com>
2020-09-27 19:31:17 +0530

    First commit!

---
+++ README.md
@@ -0,0 +1,19 @@
+<h1>Bulk Certificate Generator</h1>
+
+<p>
+![Sample Certificate](sample.jpg)
+
+**Run Command ```python certificate_generator.py```**
+
+**Required Libraries**
+- Pandas ```pip install pandas```
+- PIL   ```pip install image```
+
+**Instructions**
+* Data of bulk contacts in csv format (renamed as data.csv)
+    * only one column must have all names
+    * First row of column must be named as *name* (refer data.csv)
+* A Certificate template (template.jpg)
+* Must adjust the location of Text using *location* in code (hit and trial while producing)
+    * Use MS PAINT to determine coordinate of beginning of text in certificate (bottom left pane of ms paint)
+* You can output certificate in any format (PDF default)
---
+++ certificate_generator.py
@@ -0,0 +1,38 @@
+### make sure to rename the csv as 'data.csv' and certificate template as template.jpg
+### also works with excel sheets but pd.read_csv must be changed to pd.read_xslx
+import pandas as pd
+import os
+from PIL import Image, ImageDraw, ImageFont
+data = pd.read_csv("data.csv")
+name_list = data["name"].tolist()
+
+if  os.path.exists('output'):
+    print('output folder already exists. Certificates in it will be overwritten')
+    print('continue? Y/N')
+    confirm = input()
+    if(confirm == 'y' or confirm == 'Y'):
+        print('overwriting the data')
+    else:
+        exit(0)
+else:
+    print('Certificates will be in output folder.')
+    #name ouf the output folder
+    os.mkdir('output')
+
+print('\nProcessing....')
+for i in name_list:
+    im = Image.open("template.jpg")
+    d = ImageDraw.Draw(im)
+    text_color = (0, 0, 0)
+    # look up the windows settings -> fonts,  and type exact keyword to use installed fonts
+    font = ImageFont.truetype("arialbd.ttf", 100)
+    w, h = d.textsize(i, font)
+    horizontal = (im.width - w)
+    vertical = (im.height - h)
+    
+    # you can change location value to relocate the name. text goes right and down if x, y is increased respectively.
+    location = (horizontal/2, vertical/2 - 50)
+    d.text(location, i, anchor="north", fill = text_color, font = font)
+    # you can chnage the format to .jpg too!
+    im.save("output/certificate_" + i + ".pdf")
+print('Done!')
